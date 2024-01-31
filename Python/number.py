3a8ec142d604cf05bdd69abe738941c3bfb12c5c
Aydrean <awheeler@bostonk12.org>
2024-01-31 09:16:18 -0500

    Restored old files

---
+++ Python/number.py
@@ -0,0 +1,5 @@
+input('please enter a number')
+print('this is not a passing grade')
+if x <5:75
+input('please enter a number')
+print('this is a passing grade')
---
+++ Python/test.py
@@ -0,0 +1,1 @@
+grade=input('please enter a number')
