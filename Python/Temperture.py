1e466431f36ce61a94c801b613a3a327c068399a
Aydrean <awheeler@bostonk12.org>
2024-03-05 09:11:40 -0500

    Restored old files

---
+++ Python/Temperture.py
@@ -0,0 +1,3 @@
+Celsius=int(input("Please enter the temperature (in Celsius): "))
+Fahrenheit= 9/5*Celsius+32
+print(Celsius, "degrees Celsius is the same as" ,Fahrenheit, "degrees Fahrenheit!")
---
+++ Python/integer_input2.py
@@ -0,0 +1,4 @@
+age=int(input("please enter your age: "))
+print("you are ",age," years old!")
+x=int(input("please enter an integer "))
+print("in ",x," years, you will be" ,age+ x,"!")
