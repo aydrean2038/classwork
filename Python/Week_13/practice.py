040f7323dfd39befea1f660d79a807d525261ac3
Aydrean <awheeler@bostonk12.org>
2024-03-08 10:12:58 -0500

    practices

---
+++ Python/Week_13/practice.py
@@ -0,0 +1,4 @@
+#Aydrean Wheeler
+#march/6/2024
+x = input('Please enter your name:')
+print(f'Hi {x} I hope you are having an awesome day!')
---
+++ Python/Week_13/practice2.py
@@ -0,0 +1,13 @@
+#Aydrean Wheeler
+#march/6/2024
+
+name = input('Please enter your name:')
+food = input('Please enter your favorite food:')
+song = input('Pleas enter your favorite song:')
+quote = input('Please enter your favorite quote:')
+print(f'Information about:{name}')
+print('*****')
+print(f'favorite food {food}')
+print(f'favorite song {song}')
+print(f'favorite quote {quote}')
+print(f'Summary: {name}\'s favorite food is {food}, their favorite song is "{song}" and their favorite quote is "{quote}".')
