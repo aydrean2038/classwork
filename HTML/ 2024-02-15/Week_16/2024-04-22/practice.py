b342cadd9af8f7d03ab8bcc4e8b305a504808ec6
Aydrean <awheeler@bostonk12.org>
2024-04-23 09:37:01 -0400

    practices

---
+++ HTML/ 2024-02-15/Week_16/2024-04-22/practice.py
@@ -0,0 +1,25 @@
+# Aydrean Wheeler
+# April/22/24
+
+name1 = input("Please enter a name:")
+name2 = input("Please enter a name:")
+name3 = input("Please enter a name:")
+name4 = input("Please enter a name:")
+name5 = input("Please enter a name:")
+name6 = input("Please enter a name:")
+name7 = input("Please enter a name:")
+name8 = input("Please enter a name:")
+name9 = input("Please enter a name:")
+name10 = input("Please enter a name:")
+name11 = input("Please enter a name:")
+name12 = input("Please enter a name:")
+name13 = input("Please enter a name:")
+name14 = input("Please enter a name:")
+name15 = input("Please enter a name:")
+name16 = input("Please enter a name:")
+name17 = input("Please enter a name:")
+name18 = input("Please enter a name:")
+name19 = input("Please enter a name:")
+name20 = input("Please enter a name:")
+
+print(f"here are the names you listed:{name1}\n{name2}\n{name3}\n{name4}\n{name5}\n{name6}\n{name7}\n{name8}\n{name9}\n{name10}\n{name11}\n{name12}\n{name13}\n{name14}\n{name15}\n{name16}\n{name17}\n{name18}\n{name19}\n{name20}")
