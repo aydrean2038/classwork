52744cc46d8a030b039205a681129c37f75a6be4
Aydrean <awheeler@bostonk12.org>
2024-04-25 10:36:54 -0400

    practices

---
+++ Python/practice3.py
@@ -0,0 +1,16 @@
+#Aydrean Wheeler
+#4/25/24
+
+#this is how you make a empty list
+a=[]
+
+#this is how you creat a list with values
+b=[1,2,3]
+
+print(a)
+print(b)
+
+a+={"Winders"}
+b+=[4]
+print(a)
+print(b)
